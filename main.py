f3a4fa246126794c85db4ade12692286198f73f2
gitofficegkh <119872727+gitofficegkh@users.noreply.github.com>
2022-12-19 01:17:45 +0800

    Update main.py

--- main.py
+++ main.py
@@ -20,7 +20,7 @@ options = [
 ]
 for option in options:
     chrome_options.add_argument(option)
-    chrome_options.add_argument("–proxy-server=https://112.194.90.218:4231")
+    chrome_options.add_argument("--proxy-server=https://112.194.90.218:4231")
 
 driver = webdriver.Chrome(options=chrome_options)
 
